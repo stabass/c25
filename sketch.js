ce7464f3cebe9ccf089d0a423dc28682949273f8
stabass <shamsiya.tabassum@yahoo.com>
2021-01-07 04:53:00 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform=new Ground (120,300,300, 170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -54,4 +55,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    
 }
